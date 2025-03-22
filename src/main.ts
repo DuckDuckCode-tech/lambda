4a67257c8245dffdd0beae8308fa4b0deac83d26
declspecl <gavind2559@gmail.com>
2025-03-22 16:48:28 -0400

    extract tarball and get all files

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import * as tar from "tar";
 import { Octokit } from "octokit";
 import { DynamoDBService } from "./ddb.js";
 import { Context, Handler } from "aws-lambda";
 import { FileSystemService } from "./filesystem.js";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import path from "path";
 
 interface Payload {
     accessToken: string;
@@ -46,29 +48,35 @@ export const handler: Handler = async (payload: Payload, context: Context) => {
     const tarballPath = `${tmpDir}/${repositoryName}.tar.gz`;
     const fileSystemService = new FileSystemService();
 
-    console.log("Cleaning up previous runs and setting up local file system");
-    if (fileSystemService.existsSync(tmpDir)) fileSystemService.rmSync(tmpDir, { recursive: true, force: true });
-    if (fileSystemService.existsSync(tarballPath)) fileSystemService.unlinkSync(tarballPath);
-    await fileSystemService.createDirectory(tmpDir);
+	console.log("Cleaning up previous runs and setting up local file system");
+	if (fileSystemService.existsSync(tmpDir)) fileSystemService.rmSync(tmpDir, { recursive: true, force: true });
+	if (fileSystemService.existsSync(tarballPath)) fileSystemService.unlinkSync(tarballPath);
+	await fileSystemService.createDirectory(`${tmpDir}/source`);
 
-    console.log("Downloading tarball...");
-    const tarballUrl = `https://api.github.com/repos/${userInfo.data.login}/${repositoryName}/tarball/${repositoryBranch}`;
-    const response = await axios({
-        method: "get",
-        url: tarballUrl,
-        responseType: "stream",
-        headers: {
-            Authorization: `token ${accessToken}`,
-            Accept: "application/vnd.github.v3.raw"
-        }
-    });
+	console.log("Downloading tarball");
+	const tarballUrl = `https://api.github.com/repos/${userInfo.data.login}/${repositoryName}/tarball/${repositoryBranch}`;
+	const response = await axios({
+		method: "get",
+		url: tarballUrl,
+		responseType: "stream",
+		headers: {
+			Authorization: `token ${accessToken}`,
+			Accept: "application/vnd.github.v3.raw"
+		}
+	});
+	console.log(`User found: ${JSON.stringify(user)}`);
 
-    console.log("Saving tarball to disk...");
-    const writer = fileSystemService.createWriteStream(tarballPath);
-    await fileSystemService.pipeline(response.data, writer);
+	console.log("Saving tarball to disk");
+	const writer = fileSystemService.createWriteStream(tarballPath);
+	await fileSystemService.pipeline(response.data, writer);
 
-    console.log(`User found: ${JSON.stringify(user)}`);
+	console.log("Extracting tarball");
+	await tar.x({ file: tarballPath, cwd: `${tmpDir}/source`, strip: 1 });
 
-
+	console.log("Getting all files in source directory");
+	const sourceDir = path.join(tmpDir, "source");
+	const allFiles = await fileSystemService.getAllFilepathsInDirectory(sourceDir);
+	const relativePaths = allFiles.map(file => path.relative(sourceDir, file));
+	console.log("All files:", relativePaths);
 };
 
